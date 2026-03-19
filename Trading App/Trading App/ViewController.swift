f225fb0a93fed4cd2128dab0c5c2da1cf3995f0f
danilbeletsky <158756341+danilbeletsky@users.noreply.github.com>
2026-03-19 22:26:35 +0700

    fix

--- Trading App/Trading App/ViewController.swift
+++ Trading App/Trading App/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  Trading App
-//
-//  Created by Данил Белецкий on 17.03.2026.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
