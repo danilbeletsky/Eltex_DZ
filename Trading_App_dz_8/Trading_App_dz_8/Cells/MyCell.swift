a210e31e558651658227b3189fa43537f1477e00
danilbeletsky <158756341+danilbeletsky@users.noreply.github.com>
2026-04-11 13:41:40 +0700

    refactoring

--- Trading_App_dz_8/Trading_App_dz_8/Cells/MyCell.swift
+++ Trading_App_dz_8/Trading_App_dz_8/Cells/MyCell.swift
@@ -6,7 +6,7 @@ protocol MyCellDelegate: AnyObject {
 
 final class MyCell: UICollectionViewCell {
     // MARK: - UI Elements
-    static let identifire = "MyCell"
+    static let reuseIdentifier = "MyCell"
     
     weak var delegate: MyCellDelegate?
     private var currentCurrency: String = ""
