1255351bc93455b9fc2d0377191f1d5fcc5b04c9
danilbeletsky <158756341+danilbeletsky@users.noreply.github.com>
2026-05-25 00:28:53 +0700

    homework_21

---
+++ Homework_21/Homework_21/ContentView.swift
@@ -0,0 +1,65 @@
+import SwiftUI
+
+struct HeatmapCell: View {
+    let title: String
+    let color: Color
+    var body: some View {
+        Text(title)
+            .font(.headline)
+            .foregroundStyle(.white)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .cornerRadius(12)
+            .background(color)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 4){
+            HeatmapCell(title: "RUB", color: .red)
+            HStack{
+                VStack{
+                    HeatmapCell(title: "USD", color: .blue)
+                    HStack{
+                        HeatmapCell(title: "XCD", color: .yellow)
+                        HeatmapCell(title: "BHD", color: .orange)
+                    }
+                }
+                VStack{
+                    HStack{
+                        VStack{
+                            HeatmapCell(title: "ALL", color: .cyan)
+                            HeatmapCell(title: "BZD", color: .mint)
+                        }
+                        VStack{
+                            HeatmapCell(title: "AFN", color: .brown)
+                            HeatmapCell(title: "BYN", color: .red)
+                        }
+                    }
+                    VStack{
+                        HStack{
+                            HeatmapCell(title: "XCD", color: .gray)
+                            HeatmapCell(title: "BDT", color: .indigo)
+                        }
+                        HStack{
+                            VStack{
+                                HeatmapCell(title: "BSD", color: .pink)
+                                HeatmapCell(title: "XOF", color: .yellow)
+                            }
+                            VStack{
+                                HeatmapCell(title: "BBD", color: .green)
+                                HeatmapCell(title: "BOB", color: .blue)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.init(top: 2, leading: 2, bottom: 2, trailing: 2))
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
